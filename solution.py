36d00a0b739f5b834c222d1567755556c2b089ef
4Drammy4 <90786830+4Drammy4@users.noreply.github.com>
2023-04-13 20:18:41 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import numpy as np
 chat_id = 416934694 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    if anderson_ksamp([x, y]).significance_level < 0.1:
+    if anderson_ksamp([x, y]).significance_level < 0.07:
         return False
     else:
         return True
